f1e8cbdb218df51ac7ff9ce6ba6201e869bd2e4e
ederco2 <ederco2@gmail.com>
2021-06-21 10:30:39 -0300

    Exercicio 2

---
+++ Exercicio2While.java
@@ -0,0 +1,13 @@
+//Escreva um laço while que execute 20 vezes, imprimindo o valor da variável x
+// que inicialmente está com valor 10.
+public class Exercicio2While {
+    public static void main(String[] args){
+        int x=10;
+        int y=0;
+        while (y<=20){
+            System.out.println("X: "+x);
+            x++;
+            y++;
+        }
+    }
+}
