866da2a40ce2119e39509d7e1b7f78e9126efc34
ederco2 <ederco2@gmail.com>
2021-06-28 11:05:31 -0300

    Exercicios dia 26/06 PARTE 2

---
+++ Desafio03.java
@@ -0,0 +1,10 @@
+//Crie um programa que percorra um array de 10 posições e imprima o seu conteúdo.
+public class Desafio03 {
+    public static void main(String[] args){
+        int conteudo[]= {7,8,9};//new int[3];
+
+        for(int i=0;i<conteudo.length;i++){
+            System.out.println(conteudo[i]);
+        }
+    }
+}
